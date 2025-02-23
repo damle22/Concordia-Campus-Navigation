43a924735441c872507c600d65d1fd1226d983e0
Zachariya Javeri <zach.javeri@gmail.com>
2025-02-22 20:48:04 -0500

    Took out an unused enumeration from POIType and added class diagrams to repo

--- app/src/main/java/minicap/concordia/campusnav/buildingmanager/enumerations/POIType.java
+++ app/src/main/java/minicap/concordia/campusnav/buildingmanager/enumerations/POIType.java
@@ -1,8 +1,6 @@
 package minicap.concordia.campusnav.buildingmanager.enumerations;
 
 public enum POIType {
-    // NONE is added here as a default, it means all or none depending on context
-    NONE,
     WASHROOM,
     WATER_FOUNTAIN,
     ELEVATOR,
