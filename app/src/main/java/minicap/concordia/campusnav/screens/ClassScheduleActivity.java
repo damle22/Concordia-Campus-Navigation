a06edd8bcfdc18d81fd62343cfaebf9a910bc097
Ava-Samimi <ava.samimi@hotmail.com>
2025-03-17 18:22:00 -0400

    fix

--- app/src/main/java/minicap/concordia/campusnav/screens/ClassScheduleActivity.java
+++ app/src/main/java/minicap/concordia/campusnav/screens/ClassScheduleActivity.java
@@ -17,6 +17,7 @@ import minicap.concordia.campusnav.R;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
